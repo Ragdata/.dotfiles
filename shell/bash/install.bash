e1825d9c3abda030f5a477769f8c96b2e52ced06
Ragdata <ragdata@users.noreply.github.com>
2024-08-22 13:40:58 +1000

    feat(dotfiles): Finalised the structure of the installer

--- shell/bash/install.bash
+++ shell/bash/install.bash
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC1090
+# shellcheck disable=SC2034
 ####################################################################
 # install.bash
 ####################################################################
@@ -13,29 +15,26 @@
 ####################################################################
 # PREFLIGHT
 ####################################################################
-
+declare -x ENV_DEFAULT
+# Verify default environment file exists
+[ -f "$HOME/.dotfiles/cfg/.env.dist" ] || { echo "ERROR :: Cannot find default environment variables"; exit 1; }
+ENV_DEFAULT="$HOME/.dotfiles/cfg/.env.dist"
+# Get Environment Variables
+source "$ENV_DEFAULT"
+# Look for custom environment file and include it if it's there
+[ -f "$HOME/.env" ] && source "$HOME/.env"
+# Add critical paths to $PATH
+PATH="$BIN_DIR:$PATH"
 ####################################################################
 # INITIALIZE
 ####################################################################
-declare -x TERM_ESC TERM_CSI RED BLUE GREEN GOLD WHITE RESET
-declare -x SYMBOL_ERROR SYMBOL_WARNING SYMBOL_INFO SYMBOL_SUCCESS
+# Import critical files (the common library import other fundamental
+# libraries as well - which means we often only need to import common)
+dotImport "$FUNC_DIR/common.bash"
 #
-# TERMINAL VARIABLES
+# PATHS
 #
-TERM_ESC=$'\033'
-TERM_CSI="${TERM_ESC}["
-# COLORS
-RED="$(printf '%s31m' "$TERM_CSI")"
-BLUE="$(printf '%s94m' "$TERM_CSI")"
-GREEN="$(printf '%s32m' "$TERM_CSI")"
-GOLD="$(printf '%s33m' "$TERM_CSI")"
-WHITE="$(printf '%s97m' "$TERM_CSI")"
-RESET="$(printf '%s0m' "$TERM_CSI")"
-#  SYMBOLS
-SYMBOL_ERROR="[!]"
-SYMBOL_WARNING="[-]"
-SYMBOL_INFO="[i]"
-SYMBOL_SUCCESS="[+]"
+SHELL="$DOTFILES/shell/bash"
 #
 # ADDITIONAL VARIABLES
 #
@@ -48,85 +47,139 @@ USAGE: dot::install [OPTIONS] <args>
 # FUNCTIONS
 ####################################################################
 # ------------------------------------------------------------------
-# echoAlias
+# bash::init
 # ------------------------------------------------------------------
-if ! command -v echoAlias &> /dev/null; then
-	echoAlias()
-	{
-		local msg="${1:-}" options
-		local COLOR OUTPUT PREFIX SUFFIX STREAM=1 _0=""
-		local -a OUTARGS
+bash::init()
+{
+	local DIALOG_BACKTITLE DIALOG_TITLE DIALOG_TEXT DIALOG_INIT
 
-		[[ -z "$msg" ]] && { echo -e "${RED}${SYMBOL_ERROR} ERROR :: echoAlias requires an argument${RESET}"; exit 1; }
+	[[ ! -f "$HOME"/.dotfiles/cfg/.node ]] && cp "$HOME"/.dotfiles/cfg/.node.dist "$HOME"/.dorfiles/cfg/.node
 
-		shift
+	dotImport "$FUNC_DIR/files.bash"
 
-		options=$(getopt -l "color:,prefix:,suffix:,noline" -o "c:p:s:n" -a -- "$@")
+	file2env "$HOME"/.dotfiles/cfg/.node
 
-		eval set --"$options"
+	DIALOG_BACKTITLE="Ragdata's Dotfiles"
+	DIALOG_TITLE="NODE / USER CONFIG"
 
-		while true
-		do
-			case "$1" in
-				-c | --color)
-					COLOR="$2"
-					shift 2
-					;;
-				-p | --prefix)
-					PREFIX="$2"
-					shift 2
-					;;
-				-s | --suffix)
-					SUFFIX="$2"
-					shift 2
-					;;
-				-n | noline)
-					OUTARGS+=("-n")
-					shift
-					;;
-				--)
-					shift
-					break
-					;;
-				*)
-					echo -e "${RED}${SYMBOL_ERROR} ERROR :: echoAlias - Invalid Argument '$1'${RESET}"
-					exit 1
-					;;
-			esac
-		done
+	DIALOG_TEXT="Please confirm this node's IPv4 Address"
+	DIALOG_INIT="$(hostname -I | cut -f1 -d' ')"
 
-		[[ -n "$COLOR" ]] && _0="${RESET}"
+	[[ -z "$NODE_IPv4" ]] && { source "$HOME"/.dotfiles/lib/dialog/inputbox; export NODE_IPv4; }
+	[[ -n "$RESULT" ]] && NODE_IPv4="$RESULT"
+	[[ -n "$NODE_IPv4" ]] && sed -i "s/^$NODE_IPv4.*/NODE_IPv4=\"${NODE_IPv4}\"/" "$HOME"/.dotfiles/cfg/.node
 
-		OUTPUT="${COLOR}${PREFIX}${msg}${SUFFIX}${_0}"
+	unset DIALOG_TEXT DIALOG_INIT RESULT
 
-		if [[ "$STREAM" -eq 2 ]]; then
-			echo -e "${OUTARGS[@]}" "${OUTPUT}" >&2
-		else
-			echo -e "${OUTARGS[@]}" "$OUTPUT"
-		fi
-	}
-fi
+	DIALOG_TEXT="Please confirm the hostname of this node:"
+	DIALOG_INIT="$(hostname)"
+
+	[[ -z "$NODE_HOSTNAME" ]] && { source "$HOME"/.dotfiles/lib/dialog/inputbox; export NODE_HOSTNAME; }
+	[[ -n "$RESULT" ]] && NODE_HOSTNAME="$RESULT"
+	[[ "$RESULT" != "$DIALOG_INIT" ]] && hostname "$RESULT"
+	[[ -n "$NODE_HOSTNAME" ]] && sed -i "s/^NODE_HOSTNAME.*/NODE_HOSTNAME=\"${NODE_HOSTNAME}\"/" "$HOME"/.dotfiles/cfg/.node
+
+	unset DIALOG_TEXT DIALOG_INIT RESULT
+
+	DIALOG_TEXT="Specify the username to be used for Git commits:"
+
+	[[ -z "$GIT_USER" ]] && { source "$HOME"/.dotfiles/lib/dialog/inputbox; export GIT_USER; }
+	[[ -n "$RESULT" ]] && GIT_USER="$RESULT"
+	[[ -n "$GIT_USER" ]] && sed -i "s/^$GIT_USER.*/GIT_USER=\"${GIT_USER}\"/" "$HOME"/.dotfiles/cfg/.node
+
+	unset DIALOG_TEXT RESULT
+
+	DIALOG_TEXT="Specify the email address to be used for Git commits:"
+
+	[[ -z "$GIT_EMAIL" ]] && { source "$HOME"/.dotfiles/lib/dialog/inputbox; export GIT_EMAIL; }
+	[[ -n "$RESULT" ]] && GIT_EMAIL="$RESULT"
+	[[ -n "$GIT_EMAIL" ]] && sed -i "s/^$GIT_EMAIL.*/GIT_EMAIL=\"${GIT_EMAIL}\"/" "$HOME"/.dotfiles/cfg/.node
+
+	unset DIALOG_TEXT RESULT
+
+	DIALOG_TEXT="Specify the GPG Key ID to be used to sign Git commits:"
+
+	[[ -z "$GPG_KEYID" ]] && { source "$HOME"/.dotfiles/lib/dialog/inputbox; export GPG_KEYID; }
+	[[ -n "$RESULT" ]] && GPG_KEYID="$RESULT"
+	[[ -n "$GPG_KEYID" ]] && sed -i "s/^$GPG_KEYID.*/GPG_KEYID=\"${GPG_KEYID}\"/" "$HOME"/.dotfiles/cfg/.node
+
+	unset DIALOG_TEXT RESULT
+}
 # ------------------------------------------------------------------
-# COLOR ALIASES
+# bash::install
 # ------------------------------------------------------------------
-! command -v echoRed &> /dev/null && echoRed() { echoAlias "$1" -c "${RED}" "${@:2}"; }
-! command -v echoBlue &> /dev/null && echoBlue() { echoAlias "$1" -c "${BLUE}" "${@:2}"; }
-! command -v echoGreen &> /dev/null && echoGreen() { echoAlias "$1" -c "${GREEN}" "${@:2}"; }
-! command -v echoGold &> /dev/null && echoGold() { echoAlias "$1" -c "${GOLD}" "${@:2}"; }
-! command -v echoWhite &> /dev/null && echoWhite() { echoAlias "$1" -c "${WHITE}" "${@:2}"; }
+bash::install()
+{
+	echo ""
+}
 # ------------------------------------------------------------------
-# MESSAGE ALIASES
+# bash::uninstall
 # ------------------------------------------------------------------
-! command -v echoError &> /dev/null && echoError() { echoAlias "$SYMBOL_ERROR $1" -c "${RED}" "${@:2}"; }
-! command -v echoWarning &> /dev/null && echoWarning() { echoAlias "$SYMBOL_WARNING $1" -c "${GOLD}" "${@:2}"; }
-! command -v echoInfo &> /dev/null && echoInfo() { echoAlias "$SYMBOL_INFO $1" -c "${BLUE}" "${@:2}"; }
-! command -v echoSuccess &> /dev/null && echoSuccess() { echoAlias "$SYMBOL_SUCCESS $1" -c "${GREEN}" "${@:2}"; }
+bash::uninstall()
+{
+	echo ""
+}
 # ------------------------------------------------------------------
-# FILTERS
+# bash::update
 # ------------------------------------------------------------------
-! command -v scriptPath &> /dev/null && scriptPath() { printf '%s' "$(realpath "${BASH_SOURCE[0]}")"; }
-! command -v scriptDir &> /dev/null && scriptDir() { printf '%s' "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"; }
+bash::update()
+{
+	echo ""
+}
+# ------------------------------------------------------------------
+# bash::version
+# ------------------------------------------------------------------
+bash::version()
+{
+	echo ""
+}
+# ------------------------------------------------------------------
+# bash::quit
+# ------------------------------------------------------------------
+bash::quit()
+{
+	echo ""
+}
+# ------------------------------------------------------------------
+# bash::menu
+# ------------------------------------------------------------------
+bash::menu()
+{
+	local option status
+
+	option=$(dialog --title "BASH INSTALLER" --backtitle "Ragdata's Dotfiles Installer" --clear --default-item "1" --menu "Select from the following options:" 15 50 5 \
+		"Install" "" \
+		"Uninstall" "" \
+		"Update" "" \
+		"About" "" \
+		"Quit" "" 3>&1 1>&2 2>&3)
+
+	status=$?
+
+	if [ "$status" = 0 ]; then
+		case "$option" in
+			"Install")		bash::install;;
+			"Uninstall")	bash::uninstall;;
+			"Update")		bash::update;;
+			"About")		bash::version -v;;
+			"Quit")			bash::quit;;
+		esac
+	fi
+}
 ####################################################################
 # MAIN
 ####################################################################
+checkBash
+checkRoot
+checkDir
 
+git clone https://github.com/Ragdata/.dotfiles.git
+
+[[ -f "$HOME"/.dotfiles/cfg/.dialogrc ]] && install -v -b -C -D -t "$HOME" "$HOME"/.dotfiles/cfg/.dialogrc
+
+tput civis
+
+bash::init
+bash::menu
+
+tput cnorm

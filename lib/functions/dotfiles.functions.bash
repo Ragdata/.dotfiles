2649c2ee707438381bf045006f02745a32ab8298
Ragdata <ragdata@users.noreply.github.com>
2024-09-03 23:53:11 +1000

    feat(functions): Update dot::sysUpdate

--- lib/functions/dotfiles.functions.bash
+++ lib/functions/dotfiles.functions.bash
@@ -36,13 +36,28 @@ dot::sysUpdate()
     group 'dot'
 
 	local result
+
+	debugLog "${FUNCNAME[0]}"
+
 	echoHead "Updating Package Cache"
 	sudo apt-get -qq -y update; result=$?
-	if [ "$result" -eq 0 ]; then echoDot "OK" -c "${LT_GREEN}"; else echoDot "FAILED!" -c "${RED}"; fi
+	if [ "$result" -eq 0 ]; then 
+		log::info "Package cache updated successfully"
+		echoDot "OK" -c "${LT_GREEN}"
+	else
+		log::error "Package cache update failed"
+		echoDot "FAILED!" -c "${RED}"
+	fi
 
-	echoHead "Updating System Files"
+	echoHead "Upgrading System Files"
 	sudo apt-get -qq -y full-upgrade; result=$?
-	if [ "$result" -eq 0 ]; then echoDot "OK" -c "${LT_GREEN}"; else echoDot "FAILED!" -c "${RED}"; fi
+	if [ "$result" -eq 0 ]; then 
+		log::info "System files upgraded successfully"
+		echoDot "OK" -c "${LT_GREEN}"
+	else
+		log::error "System files upgrade failed"
+		echoDot "FAILED!" -c "${RED}"
+	fi
 }
 # ------------------------------------------------------------------
 # dot::update::bin

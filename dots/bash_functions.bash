557de883ea00a775c4f2f4702bfc9e3f4580ea51
Ragdata <ragdata@users.noreply.github.com>
2024-09-03 10:30:42 +1000

    feat: Add common functions

--- dots/bash_functions.bash
+++ dots/bash_functions.bash
@@ -11,3 +11,120 @@
 # Repository:		https://github.com/Ragdata/.dotfiles
 # Copyright:    Copyright © 2024 Redeyed Technologies
 ####################################################################
+# COMMON FUNCTIONS
+####################################################################
+# ------------------------------------------------------------------
+# errorExit
+# ------------------------------------------------------------------
+errorExit()
+{
+	local msg="ERROR :: " code="${2:-1}"
+
+	[[ -n "${FUNCNAME[1]}" ]] && msg+=" ${FUNCNAME[1]} ::"
+	[[ -n "$1" ]] && msg+=" $1"
+
+	echoError "$msg"
+	exit "$code"
+}
+# ------------------------------------------------------------------
+# checkBash
+# ------------------------------------------------------------------
+checkBash() { if [[ "${BASH_VERSION:0:1}" -lt 4 ]]; then errorExit "This script requires a minimum Bash version of 4+"; fi }
+####################################################################
+# ARRAY FUNCTIONS
+####################################################################
+# ------------------------------------------------------------------
+# arr::contains
+# ------------------------------------------------------------------
+arr::contains()
+{
+	local e val="$1"
+
+	shift
+
+	for e; do [[ "$e" == "$val" ]] && return 0; done
+
+	return 1
+}
+####################################################################
+# DOT FUNCTIONS
+####################################################################
+# ------------------------------------------------------------------
+# dot::source
+# ------------------------------------------------------------------
+dot::source()
+{
+	(($# > 0)) || errorExit "dot::source - Missing Arguments"
+
+	[ -f "$1" ] || errorExit "dot::source - File '$1' not found"
+
+	if arr::contains "$1" "${IMPORTS[@]}"; then
+		return 0
+	else
+		IMPORTS+=("$1")
+		# shellcheck disable=SC1090
+		source "$1" || return 1
+	fi
+
+	return 0
+}
+# ------------------------------------------------------------------
+# dot::include
+# ------------------------------------------------------------------
+dot::include()
+{
+	(($# > 0)) || errorExit "dot::include - Missing Arguments"
+
+	local -a ARR
+	local path
+
+	for item in "$@"
+	do
+		ARR=()
+		path=""
+		stub=""
+		if [ -f "$item" ]; then
+			path="$item"
+		elif [ -f "$PKGS/$item" ]; then
+			path="$PKGS/$item"
+		elif [[ "$item" == *"."* ]]; then
+			mapfile -d "." -t ARR < <(printf '%s' "$item")
+		else
+			errorExit "dot::include - Unrecognised item '$item'"
+		fi
+		if [ "${#ARR[@]}" -gt 0 ]; then
+			case "${ARR[1]}" in
+				alias|aliases)
+					stub="aliases/$item.bash"
+					;;
+				completion|completions)
+					stub="completions/$item.bash"
+					;;
+				custom)
+					path="$CUSTOM/$item.bash"
+					;;
+				function|functions)
+					stub="functions/$item.bash"
+					;;
+				plugin|plugins)
+					stub="plugins/$item.bash"
+					;;
+				*)	errorExit "dot::include - Type '${ARR[1]}' not supported";;
+			esac
+		fi
+		if [ -n "$stub" ]; then
+			if [ -f "$CUSTOM/$stub" ]; then
+				path="$CUSTOM/$stub"
+			elif [ -f "$DOT_LIB/$stub" ]; then
+				path="$DOT_LIB/$stub"
+			else
+				errorExit "dot::include - No match for '$stub'"
+			fi
+		fi
+		if [ -f "$path" ]; then
+			dot::source "$path"
+		else
+			errorExit "dot::include - File '$path' not found"
+		fi
+	done
+}

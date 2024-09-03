cd546f817fdd0a2da565dd7424a35053daad9fa0
Ragdata <ragdata@users.noreply.github.com>
2024-09-03 15:53:08 +1000

    refactor: Adjust indent value

--- dots/bash_completions.bash
+++ dots/bash_completions.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC1091
 ####################################################################
 # .bash_completions
 ####################################################################
@@ -16,9 +17,9 @@
 # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
 # sources /etc/bash.bashrc).
 if ! shopt -oq posix; then
-  if [ -f /usr/share/bash-completion/bash_completion ]; then
-    . /usr/share/bash-completion/bash_completion
-  elif [ -f /etc/bash_completion ]; then
-    . /etc/bash_completion
-  fi
+    if [ -f /usr/share/bash-completion/bash_completion ]; then
+        . /usr/share/bash-completion/bash_completion
+    elif [ -f /etc/bash_completion ]; then
+        . /etc/bash_completion
+    fi
 fi

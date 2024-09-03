122c95c387cb00090cf061f0255a031445a0230e
Ragdata <ragdata@users.noreply.github.com>
2024-09-04 02:07:55 +1000

    fix: Type mismatch in log::checkLog

--- lib/functions/log.functions.bash
+++ lib/functions/log.functions.bash
@@ -67,7 +67,7 @@ log::checkLog()
     # check logfile size
     size="$(wc -c "$DOT_LOG/$fileName.log" | awk '{print $1}')"
     # rotate if necessary
-    [ "$size" -ge "$LOG_SIZE" ] && log::rotate "$fileName"
+    ((size >= LOG_SIZE)) && log::rotate "$fileName"
 }
 # ------------------------------------------------------------------
 # log::init

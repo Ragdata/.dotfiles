7e6e3e0cb4bf8762338fc80ec6e2a91b72a42e83
Ragdata <ragdata@users.noreply.github.com>
2024-09-06 02:52:04 +1000

    feat: Add apps

---
+++ lib/apps/harden/harden.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090
+# shellcheck disable=SC2034
+####################################################################
+# harden.bash
+####################################################################
+# Ragdata's Dotfiles - Function Definitions
+#
+# File:         harden.bash
+# Author:       Ragdata
+# Date:         04/09/2024
+# License:      MIT License
+# Repository:	https://github.com/Ragdata/.dotfiles
+# Copyright:    Copyright © 2024 Redeyed Technologies
+####################################################################
+# ATTRIBUTION
+####################################################################
+# Much of the code in this script I shamelessly ripped off from
+# The Hardening Ubuntu project - Copyright 2018 Thomas Sjögren
+# https://github.com/konstruktoid/hardening
+####################################################################
+# DEPENDENCIES
+####################################################################
+# required library files
+dot::include "log.functions"
+####################################################################
+# HARDENING FUNCTIONS
+####################################################################
---
+++ lib/apps/label-manager/label-manager.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090
+# shellcheck disable=SC2034
+####################################################################
+# label-manager.bash
+####################################################################
+# Ragdata's Dotfiles - Function Definitions
+#
+# File:         label-manager.bash
+# Author:       Ragdata
+# Date:         04/09/2024
+# License:      MIT License
+# Repository:	https://github.com/Ragdata/.dotfiles
+# Copyright:    Copyright © 2024 Redeyed Technologies
+####################################################################
+# DEPENDENCIES
+####################################################################
+# required library files
+dot::include "log.functions"
+####################################################################
+# HARDENING FUNCTIONS
+####################################################################

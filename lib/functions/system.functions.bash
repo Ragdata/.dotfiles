cbd608109d0a38bed12d27e468d429d6e06324d9
Ragdata <ragdata@users.noreply.github.com>
2024-09-06 02:51:34 +1000

    feat: Add system functions file

---
+++ lib/functions/system.functions.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090
+# shellcheck disable=SC2034
+####################################################################
+# system.functions
+####################################################################
+# Ragdata's Dotfiles - Function Definitions
+#
+# File:         system.functions
+# Author:       Ragdata
+# Date:         22/08/2024
+# License:      MIT License
+# Repository:	https://github.com/Ragdata/.dotfiles
+# Copyright:    Copyright © 2024 Redeyed Technologies
+####################################################################
+# DEPENDENCIES
+####################################################################
+# required library files
+dot::include "log.functions"
+####################################################################
+# SYSTEM FUNCTIONS
+####################################################################

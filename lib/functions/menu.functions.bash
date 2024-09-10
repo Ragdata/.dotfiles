385309c5d318147be54e870776e7a48360537519
Ragdata <ragdata@users.noreply.github.com>
2024-09-10 22:41:26 +1000

    style(menu): Update main menu

--- lib/functions/menu.functions.bash
+++ lib/functions/menu.functions.bash
@@ -48,7 +48,7 @@ menu()
 
 	result=$(dialog --ok-label "${OK_LABEL:-"OK"}" --cancel-label "${CANCEL_LABEL:-"Cancel"}" \
 		--backtitle "${DIALOG_BACKTITLE}" --title "${DIALOG_TITLE}" --clear \
-		--menu "${DIALOG_TEXT}" "${HEIGHT:-20}" "${WIDTH:-50}" "${MENU_HEIGHT:-9}" \
+		--menu "${DIALOG_TEXT}" "${HEIGHT:-18}" "${WIDTH:-50}" "${MENU_HEIGHT:-9}" \
 		"${DIALOG_OPTIONS[@]}" 3>&1 1>&2 2>&3)
 
 	status=$?

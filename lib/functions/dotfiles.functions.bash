be06e85203d47a7d9dca8a263640a047fdf4166e
Ragdata <ragdata@users.noreply.github.com>
2024-09-11 12:55:40 +1000

    feat(config): Add custom node config

--- lib/functions/dotfiles.functions.bash
+++ lib/functions/dotfiles.functions.bash
@@ -203,29 +203,29 @@ dot::update::sys()
 {
     group 'dot'
 
-	local result
+    local result
 
-	debugLog "${FUNCNAME[0]}"
+    debugLog "${FUNCNAME[0]}"
 
-	echoHead "Updating Package Cache"
-	sudo apt-get -qq -y update; result=$?
-	if [ "$result" -eq 0 ]; then
-		log::info "Package cache updated successfully"
-		echoDot "OK" -c "${LT_GREEN}"
-	else
-		log::error "Package cache update failed"
-		echoDot "FAILED!" -c "${RED}"
-	fi
+    echoHead "Updating Package Cache"
+    sudo apt-get -qq -y update; result=$?
+    if [ "$result" -eq 0 ]; then
+        log::info "Package cache updated successfully"
+        echoDot "OK" -c "${LT_GREEN}"
+    else
+        log::error "Package cache update failed"
+        echoDot "FAILED!" -c "${RED}"
+    fi
 
-	echoHead "Upgrading System Files"
-	sudo apt-get -qq -y full-upgrade; result=$?
-	if [ "$result" -eq 0 ]; then
-		log::info "System files upgraded successfully"
-		echoDot "OK" -c "${LT_GREEN}"
-	else
-		log::error "System files upgrade failed"
-		echoDot "FAILED!" -c "${RED}"
-	fi
+    echoHead "Upgrading System Files"
+    sudo apt-get -qq -y full-upgrade; result=$?
+    if [ "$result" -eq 0 ]; then
+        log::info "System files upgraded successfully"
+        echoDot "OK" -c "${LT_GREEN}"
+    else
+        log::error "System files upgrade failed"
+        echoDot "FAILED!" -c "${RED}"
+    fi
 }
 # ------------------------------------------------------------------
 # dot::update::bin
@@ -234,25 +234,39 @@ dot::update::bin()
 {
     group 'dot'
 
-	local fileName result
+    local fileName result
 
-	debugLog "${FUNCNAME[0]}"
+    debugLog "${FUNCNAME[0]}"
 
-	echoHead "Updating .dotfiles binaries"
-	while IFS= read -r file
-	do
-		fileName="$(basename "$file")"
-		echoDot "$fileName - " -n
-		[ -e "/usr/local/bin/$fileName" ] && sudo rm -f "/usr/local/bin/$fileName"
-		sudo ln -s "$file" "/usr/local/bin/$fileName"; result=$?
-		if [ "$result" -eq 0 ]; then
-			log::info "'$fileName' linked successfully"
-			echoAlias "OK" -c "${LT_GREEN}"
-		else
-			log::error "'$fileName' link failed"
-			echoAlias "FAILED!" -c "${RED}"
-		fi
-	done < <(find "$DOT_BIN" -type f)
+    echoHead "Updating .dotfiles binaries"
+    while IFS= read -r file
+    do
+        fileName="$(basename "$file")"
+        echoDot "$fileName - " -n
+        [ -e "/usr/local/bin/$fileName" ] && sudo rm -f "/usr/local/bin/$fileName"
+        sudo ln -s "$file" "/usr/local/bin/$fileName"; result=$?
+        if [ "$result" -eq 0 ]; then
+            log::info "'$fileName' linked successfully"
+            echoAlias "OK" -c "${LT_GREEN}"
+        else
+            log::error "'$fileName' link failed"
+            echoAlias "FAILED!" -c "${RED}"
+        fi
+    done < <(find "$DOT_BIN" -type f)
+}
+# ------------------------------------------------------------------
+# dot::update::config
+# ------------------------------------------------------------------
+dot::update::config()
+{
+    group 'dot'
+
+    debugLog "${FUNCNAME[0]}"
+
+    echoHead "Updating config"
+
+    [ -d "$CUSTOM/cfg" ] || { mkdir -p "$CUSTOM/cfg" || exitLog "Unable to create directory '$CUSTOM/cfg'"; }
+    [ -f "$CUSTOM/cfg/.node" ] || { install -v -m 0644 -C -D -t "$CUSTOM/cfg/.node" "$DOT_CFG/" || exitLog "Unable to install '$CUSTOM/cfg/.node'"; }
 }
 # ------------------------------------------------------------------
 # dot::update::dots
@@ -261,37 +275,37 @@ dot::update::dots()
 {
     group 'dot'
 
-	local -a DOT=()
+    local -a DOT=()
 
-	debugLog "${FUNCNAME[0]}"
+    debugLog "${FUNCNAME[0]}"
 
-	echoHead "Updating dotfiles"
+    echoHead "Updating dotfiles"
 
-	[ -d "$HOME/.backup" ] || { mkdir -p "$HOME/.backup" || exitLog "Unable to create directory '$HOME/.backup'" "error"; }
-	[ -L "$HOME/.profile" ] && rm -f "$HOME/.profile"
-	[ -f "$HOME/.profile" ] && mv -b "$HOME/.profile" "$HOME/.backup/.profile"
+    [ -d "$HOME/.backup" ] || { mkdir -p "$HOME/.backup" || exitLog "Unable to create directory '$HOME/.backup'"; }
+    [ -L "$HOME/.profile" ] && rm -f "$HOME/.profile"
+    [ -f "$HOME/.profile" ] && mv -b "$HOME/.profile" "$HOME/.backup/.profile"
 
-	while IFS= read -r file
-	do
-		DOT=()
-		fileName="$(basename "$file")"
-		[ -f "$CUSTOM/dots/$fileName" ] && file="$CUSTOM/dots/$fileName"
-		mapfile -d "." -t DOT < <(printf '%s' "$fileName")
-		echoDot ".${DOT[0]} - " -n
-		if [ -L "$HOME/.${DOT[0]}" ]; then
-			rm -f "$HOME/.${DOT[0]}"
-		elif [ -f "$HOME/.${DOT[0]}" ]; then
-			mv -b "$HOME/.${DOT[0]}" "$HOME/.backup/.${DOT[0]}"
-		fi
-		ln -s "$file" "$HOME/.${DOT[0]}"; result=$?
-		if [ "$result" -eq 0 ]; then
-			log::info "'.${DOT[0]}' linked successfully"
-			echoAlias "OK" -c "${LT_GREEN}"
-		else
-			log::error "'.${DOT[0]}' link failed"
-			echoAlias "FAILED!" -c "${RED}"
-		fi
-	done < <(find "$DOTS" -maxdepth 1 -type f)
+    while IFS= read -r file
+    do
+        DOT=()
+        fileName="$(basename "$file")"
+        [ -f "$CUSTOM/dots/$fileName" ] && file="$CUSTOM/dots/$fileName"
+        mapfile -d "." -t DOT < <(printf '%s' "$fileName")
+        echoDot ".${DOT[0]} - " -n
+        if [ -L "$HOME/.${DOT[0]}" ]; then
+            rm -f "$HOME/.${DOT[0]}"
+        elif [ -f "$HOME/.${DOT[0]}" ]; then
+            mv -b "$HOME/.${DOT[0]}" "$HOME/.backup/.${DOT[0]}"
+        fi
+        ln -s "$file" "$HOME/.${DOT[0]}"; result=$?
+        if [ "$result" -eq 0 ]; then
+            log::info "'.${DOT[0]}' linked successfully"
+            echoAlias "OK" -c "${LT_GREEN}"
+        else
+            log::error "'.${DOT[0]}' link failed"
+            echoAlias "FAILED!" -c "${RED}"
+        fi
+    done < <(find "$DOTS" -maxdepth 1 -type f)
 }
 # ------------------------------------------------------------------
 # dot::update::repo
@@ -324,6 +338,7 @@ dot::update()
 	debugLog "${FUNCNAME[0]}"
 
 	dot::update::repo
+	dot::update::config
 	dot::update::bin
 	dot::update::dots
 

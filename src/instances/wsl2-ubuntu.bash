93e046b8e8feb7c231644149c3429abe1f4d733a
Ragdata <ragdata@users.noreply.github.com>
2024-09-16 00:17:26 +1000

    feat(instances): Update wsl2-ubuntu instance script

---
+++ src/instances/wsl2-ubuntu.bash
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090
+# shellcheck disable=SC2034
+####################################################################
+# wsl2-ubuntu.bash
+####################################################################
+# Ragdata's Dotfiles - Function Definitions
+#
+# File:         wsl2-ubuntu.bash
+# Author:       Ragdata
+# Date:         15/09/2024
+# License:      MIT License
+# Repository:	https://github.com/Ragdata/.dotfiles
+# Copyright:    Copyright © 2024 Redeyed Technologies
+####################################################################
+# DEPENDENCIES
+####################################################################
+# required library files
+dot::include "dotfiles.functions"
+####################################################################
+# VARS
+####################################################################
+REPOS=("ppa:ansible/ansible" "ppa:wslutilities/wslu")
+####################################################################
+# MAIN
+####################################################################
+pkgAddRepos "${REPOS[@]}"
+
+pkg::installList "dependencies"
+pkg::removeList "purge"
+
+pkgInstall "shellcheck" "ubuntu-wsl" "wslu"
+
+dot::launch::script "disablenet"
+dot::launch::script "disablefs"
+dot::launch::script "disablemod"
+dot::launch::script "systemdconf"
+dot::launch::script "logindconf"
+dot::launch::script "timesyncd"
+dot::launch::script "fstab"
+dot::launch::script "hosts"
+dot::launch::script "issue"
+dot::launch::script "logindefs"
+dot::launch::script "sysctl"
+dot::launch::script "limits"
+dot::launch::script "user"
+dot::launch::script "sshd"
+dot::launch::script "password"
+dot::launch::script "cron"
+dot::launch::script "ctrlaltdel"
+dot::launch::script "rhosts"
+dot::launch::script "compilers"
+dot::launch::script "wsl"

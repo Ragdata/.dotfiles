21ade951c13f92bfc7d1c0ca2ff0636088d769d7
Ragdata <ragdata@users.noreply.github.com>
2024-09-16 11:22:11 +1000

    feat(dots): Include enabled aliases

--- dots/bash_aliases.bash
+++ dots/bash_aliases.bash
@@ -40,3 +40,12 @@ alias l='ls -CF'
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+# Load all enabled alias files
+if [ -f "$DOT_REG/aliases.enabled" ]; then
+    while IFS= read -r file
+    do
+        # shellcheck disable=SC1090
+        source "$file"
+    done < "$DOT_REG/aliases.enabled"
+fi

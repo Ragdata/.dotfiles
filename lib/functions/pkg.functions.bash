c3027bf42a6db4f11e6a665e65669d6bc66c0437
Ragdata <ragdata@users.noreply.github.com>
2024-09-16 14:23:21 +1000

    chore: Minor update

--- lib/functions/pkg.functions.bash
+++ lib/functions/pkg.functions.bash
@@ -330,7 +330,8 @@ pkg::install()
 		func="$pkg::install"
 		if [[ $(type -t "$func") == "function" ]]; then
 			echoDot "Installing '$pkg' - " -s "✚" -n
-			eval "$func" "$gc"; result=$?; tested=1
+			#eval "$func" "$gc"; result=$?; tested=1
+			result=$("$func" "$gc"); tested=1
 		fi
 	fi
 

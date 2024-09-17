94b826aaa242248434d4961d0b9a937c47032ae9
Ragdata <ragdata@users.noreply.github.com>
2024-09-17 19:25:21 +1000

    refactor: Remove string aliases

--- lib/aliases/general.aliases.bash
+++ lib/aliases/general.aliases.bash
@@ -24,8 +24,3 @@ fi
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
-#-------------------------------------------------------------------
-# STRING ALIASES
-#-------------------------------------------------------------------
-alias toLower='str::toLower'
-alias toUpper='str::toUpper'

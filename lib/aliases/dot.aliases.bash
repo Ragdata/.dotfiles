56c2c1e3341c9c730a11ea52e11cd0a711ec5d11
Ragdata <ragdata@users.noreply.github.com>
2024-09-17 18:16:21 +1000

    feat: Add aliases

--- lib/aliases/dot.aliases.bash
+++ lib/aliases/dot.aliases.bash
@@ -17,7 +17,4 @@ group 'aliases'
 alias dotfiles="source $DOT_BIN/dotfiles"
 alias dotgit="source $DOT_BIN/dotgit"
 alias dotlaunch="source $DOT_BIN/dotlaunch"
-
-alias .files="dotfiles"
-alias .git="dotgit"
-alias .launch="dotlaunch"
+alias dotssh="source $DOT_BIN/dotssh"

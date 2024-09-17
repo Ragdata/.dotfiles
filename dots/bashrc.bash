443f6ea03a4949a7aaea36e7dce3d47ce84ebee7
Ragdata <ragdata@users.noreply.github.com>
2024-09-18 03:03:07 +1000

    feat: Add LC_ALL to .bashrc

--- dots/bashrc.bash
+++ dots/bashrc.bash
@@ -23,6 +23,8 @@ case $- in
       *) return;;
 esac
 
+export LC_ALL="C"
+
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
 

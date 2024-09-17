a0dea36d9691e793343b461f04469ad22af562b9
Ragdata <ragdata@users.noreply.github.com>
2024-09-18 09:35:32 +1000

    feat: Add wsl2-kali instance file

---
+++ src/instances/wsl2-kali.bash
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090
+# shellcheck disable=SC2034
+####################################################################
+# wsl2-kali.bash
+####################################################################
+# Ragdata's Dotfiles - Function Definitions
+#
+# File:         wsl2-kali.bash
+# Author:       Ragdata
+# Date:         15/09/2024
+# License:      MIT License
+# Repository:	https://github.com/Ragdata/.dotfiles
+# Copyright:    Copyright © 2024 Redeyed Technologies
+####################################################################
+# DEPENDENCIES
+####################################################################
+# required config files
+source "$CUSTOM/cfg/.node"
+# required library files
+dot::include "dotfiles.functions"
+####################################################################
+# METADATA
+####################################################################
+about 'An instance commissioning script for systems running Kali on WSL2'
+group 'instances'
+####################################################################
+# MAIN
+####################################################################
+clear
+
+dot::install::repos
+pkgAddRepos "ppa:ansible/ansible" "ppa:wslutilities/wslu"
+echo ""
+
+echoDot "Processing purge list" -s "➤" -c "${GOLD}"
+pkg::removeList "purge"
+echo ""
+
+dot::install::deps
+pkgInstall "shellcheck" "wslu"
+
+echo ""
+echoDot "Hardening instance" -s "➤" -c "${GOLD}"
+script::launch "disablenet"
+script::launch "disablefs"
+script::launch "disablemod"
+script::launch "systemdconf"
+script::launch "logindconf"
+script::launch "timesyncd"
+script::launch "fstab"
+script::launch "hosts"
+script::launch "issue"
+script::launch "logindefs"
+script::launch "sysctl"
+script::launch "limits"
+script::launch "user"
+script::launch "sshd"
+script::launch "password"
+script::launch "ctraltdel"
+script::launch "rhosts"
+script::launch "compilers"
+script::launch "wsl"
+echo ""
+pkg::cleanup

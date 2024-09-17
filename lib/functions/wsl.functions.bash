dd221e5e279b5215b844a36c8d92735c3c89343a
Ragdata <ragdata@users.noreply.github.com>
2024-09-17 22:34:25 +1000

    feat: Add provision for windows-based .ssh skeleton directory

--- lib/functions/wsl.functions.bash
+++ lib/functions/wsl.functions.bash
@@ -14,3 +14,27 @@
 ####################################################################
 # WSL FUNCTIONS
 ####################################################################
+#-------------------------------------------------------------------
+# wsl::ssh::init
+#-------------------------------------------------------------------
+wsl::ssh::init()
+{
+    group 'ssh'
+
+    log::debug "${FUNCNAME[0]}"
+
+    local gpg_key
+
+    [ -d "$WIN_HOME/.ssh-skel" ] || return 0
+    [ -d "$WSL_HOME/.ssh" ] || mkdir -p "$WSL_HOME/.ssh"
+
+    cp "$WIN_HOME/.ssh-skel/*" "$WSL_HOME/.ssh/."
+
+    gpg_key="$(find "$WSL_HOME/.ssh" -type f -name "*_SECRET.asc")"
+
+    if [ -f "$gpg_key" ]; then
+        if gpg --import "$gpg_key"; then
+            rm -f "$gpg_key"
+        fi
+    fi
+}

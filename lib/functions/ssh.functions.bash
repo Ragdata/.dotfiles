9dad3cb1265905d26499bdbafbbe2af17c6b60e5
Ragdata <ragdata@users.noreply.github.com>
2024-09-17 17:36:50 +1000

    style: Minor cosmetic update

---
+++ lib/functions/ssh.functions.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090
+# shellcheck disable=SC2034
+####################################################################
+# ssh.functions
+####################################################################
+# Ragdata's Dotfiles - Function Definitions
+#
+# File:         ssh.functions
+# Author:       Ragdata
+# Date:         22/08/2024
+# License:      MIT License
+# Repository:	https://github.com/Ragdata/.dotfiles
+# Copyright:    Copyright © 2024 Redeyed Technologies
+####################################################################
+# DEPENDENCIES
+####################################################################
+# required library files
+dot::include "log.functions"
+####################################################################
+# SSH FUNCTIONS
+####################################################################
+#-------------------------------------------------------------------
+# alias::disable
+#-------------------------------------------------------------------

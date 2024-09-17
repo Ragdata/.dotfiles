0ace2fb0f0bf50c733036c4d9f33c4b87d716597
Ragdata <ragdata@users.noreply.github.com>
2024-09-17 12:24:08 +1000

    feat: Add component loaders

--- dots/bash_completions.bash
+++ dots/bash_completions.bash
@@ -12,7 +12,11 @@
 # Repository:	https://github.com/Ragdata/.dotfiles
 # Copyright:    Copyright © 2024 Redeyed Technologies
 ####################################################################
-
+# DEPENDENCIES
+####################################################################
+# required library files
+dot::include "completion.functions"
+####################################################################
 # enable programmable completion features (you don't need to enable
 # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
 # sources /etc/bash.bashrc).
@@ -22,4 +26,14 @@ if ! shopt -oq posix; then
     elif [ -f /etc/bash_completion ]; then
         . /etc/bash_completion
     fi
+fi
+# load all enabled completion files
+if [ -f "$DOT_REG/completions.enabled" ]; then
+    while IFS= read -r file
+    do
+        # shellcheck disable=SC1090
+        if [[ "${file:0:1}" != "#" && -n "$file" ]]; then
+            [ -f "$file" ] && source "$file"
+        fi
+    done < "$DOT_REG/completions.enabled"
 fi
--- dots/bash_plugins.bash
+++ dots/bash_plugins.bash
@@ -19,3 +19,13 @@ dot::include "plugin.functions"
 ####################################################################
 # PLUGINS TO LOAD
 ####################################################################
+# load all enabled plugin files
+if [ -f "$DOT_REG/plugins.enabled" ]; then
+    while IFS= read -r file
+    do
+        # shellcheck disable=SC1090
+        if [[ "${file:0:1}" != "#" && -n "$file" ]]; then
+            [ -f "$file" ] && source "$file"
+        fi
+    done < "$DOT_REG/plugins.enabled"
+fi

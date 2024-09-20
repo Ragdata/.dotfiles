7947389e5d228ade6f28225588f39ea54b17ec9e
Ragdata <ragdata@users.noreply.github.com>
2024-09-21 09:39:47 +1000

    chore: Minor updates

--- src/instances/helpers/wsl.bash
+++ src/instances/helpers/wsl.bash
@@ -16,10 +16,13 @@ group 'helpers'
 ####################################################################
 if [ "$1" != "rebooted" ]; then
     echoDot "Configuring WSL2" -s "➤" -c "${GOLD}"
+
     script::launch "fstab"
+
     echoDot "Install /etc/wsl.conf" -s "▶"
     if [ -f "$CUSTOM/etc/wsl.conf" ]; then source="$CUSTOM/etc/wsl.conf"; else source="$DOT_ETC/wsl.conf"; fi
     if [ -f "$source" ]; then sudo install -b -C -m 0644 -T "$source" /etc/wsl.conf; fi
+
     echoDot "Write reboot command" -s "▶"
     echo "source \"$INSTANCES/wsl2-ubuntu.bash\" rebooted" >> "$DOTS/bashrc.bash"
     echo ""
@@ -34,12 +37,15 @@ else
     cd "$DOTFILES" || exitLog "Unable to 'cd $DOTFILES'"
     git reset --hard
     cd - || exitLog "Unable to return to previous directory"
+
     echo ""
     echoDot "Configuring WSL2" -s "➤" -c "${GOLD}"
     if [ -f "$CUSTOM/cfg/.wslconfig" ]; then source="$CUSTOM/cfg/.wslconfig"; else source="$DOT_CFG/.wslconfig"; fi
     if [ -f "$source" ]; then sudo install -b -C -m 0644 -T "$source" "$WIN_HOME/.wslconfig"; fi
-    script::launch "systemd-resolved"
+
     echo ""
+    script::launch "systemd-resolved"
+
     echoDot "Copying .ssh-skel" -s "▶"
     if [ ! -d "$HOME/.ssh" ]; then mkdir -p "$HOME/.ssh" || exitLog "Unable to create '$HOME/.ssh'"; fi
     while IFS= read -r file
@@ -47,7 +53,7 @@ else
         if [[ "$(basename "$file")" == *.pub || "$(basename "$file")" == *config ]]; then perm="0644"; else perm="0600"; fi
         install -C -m "$perm" -T "$file" "$HOME/.ssh/$(basename "$file")"
     done < <(find "$WIN_HOME/.ssh-skel" -type f)
-    echo ""
+
     echoDot "Importing GPG key" -s "▶"
     gpg_key="$(find "$HOME/.ssh" -type f -name "*_SECRET.asc")"
     if [ -f "$gpg_key" ]; then
@@ -55,11 +61,14 @@ else
             rm -f "$gpg_key"
         fi
     fi
+
     echo ""
-    echoDot "Waiting for services to boot ..." -s "➤" -c "${GOLD}"
-    until resolvectl query google.com &> /dev/null
-    do
-        sleep 2
-        printf -- '%s' "."
-    done
+    if ! resolvectl query google.com; then
+        echoDot "Waiting for services to boot ..." -s "➤" -c "${GOLD}"
+        until resolvectl query google.com &> /dev/null
+        do
+            sleep 2
+            printf -- '%s' "."
+        done
+    fi
 fi

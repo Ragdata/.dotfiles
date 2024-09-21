fc2cb172c61f5272622d532e39096a9c9ed2c437
Ragdata <ragdata@users.noreply.github.com>
2024-09-21 12:11:22 +1000

    fix: Fix syntax error

--- lib/functions/git.functions.bash
+++ lib/functions/git.functions.bash
@@ -150,7 +150,7 @@ git::subtree::remove()
 
     # shellcheck disable=SC2091
     if $(yq 'has("subtree")' "$TREEFILE"); then
-        if $(yq ".subtree | has($name)" "$TREEFILE"); then
+        if $(yq ".subtree | has('$name')" "$TREEFILE"); then
             if $(yq ".subtree.$name | has('path')" "$TREEFILE"); then path="$(yq ".subtree.$name.path" "$TREEFILE")"; else exitLog "Subtree has no path"; fi
         else
             exitLog "Subtree '$name' not found"

a331c9b4d88a23c904ac8a67bc91e50292078fe3
Ragdata <ragdata@users.noreply.github.com>
2024-10-13 10:15:40 +1000

    feat: Add python package

---
+++ lib/aliases/python.aliases.bash
@@ -0,0 +1,17 @@
+# shellcheck shell=bash
+# shellcheck disable=SC2139
+####################################################################
+# python.aliases.bash
+####################################################################
+# License:      MIT License
+# Repository:	https://github.com/Ragdata/.dotfiles
+# Copyright:    Copyright © 2024 Redeyed Technologies
+####################################################################
+# METADATA
+####################################################################
+about 'Python3 aliases'
+group 'aliases'
+####################################################################
+# ALIASES
+####################################################################
+alias py="pythonpy"

1e1ec08abce85633998f0685b92c552d6cf951d7
Ragdata <ragdata@users.noreply.github.com>
2024-10-18 15:54:35 +1000

    style: Add details to setup.py

--- lib/plugins/label-manager/setup.py
+++ lib/plugins/label-manager/setup.py
@@ -5,6 +5,9 @@ setup(
     version="0.1.0",
     description="Command-line tool for managing GitHub labels",
     packages=["label_manager"],
+    author="Ragdata",
+    author_email="ragdata@users.noreply.github.com",
+    url="https://github.com/ragdata/.dotfiles",
     install_requires=["PyGithub==2.4.0"],
     entry_points={
         "console_scripts": ['label_manager=label_manager.src.cli:run']

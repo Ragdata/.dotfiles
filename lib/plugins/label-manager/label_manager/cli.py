3dcdd7eee3eda3b83de680f4ec452490e4a5d97b
Ragdata <ragdata@users.noreply.github.com>
2024-10-18 19:57:57 +1000

    perf: Update label-manager

--- lib/plugins/label-manager/label_manager/cli.py
+++ lib/plugins/label-manager/label_manager/cli.py
@@ -1,10 +1,13 @@
 import argparse
+import pathlib
 import logging
+import random
 import json
 import sys
 import os
+import re
 
-from label_manager import msg
+from label_manager import manager, msg
 
 def _parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
@@ -14,12 +17,35 @@ def _parser() -> argparse.ArgumentParser:
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
 
-    flags = parser.add_mutually_exclusive_group()
+    flags = parser.add_argument_group('flags')
+    flags_mex = flags.add_mutually_exclusive_group()
+    # optional = parser.add_argument_group('optional')
+    # required = parser.add_argument_group('required')
 
     # cli flags
-    flags.add_argument('-h', '--help', action='help', help='Show this help message and exit')
-    flags.add_argument('-V', '--version', action='version', version="%(prog)s v0.1.0", help='Display module version and exit')
-    flags.add_argument('-v', '--validate', action='store_true', help='Validate local label definitions')
+    flags_mex.add_argument('-h', '--help', action='help', help='Show this help message and exit')
+    flags_mex.add_argument('-V', '--version', action='version', version="%(prog)s v0.1.0", help='Display module version and exit')
+    flags_mex.add_argument('-v', '--validate', action='store_true', help='Validate local label definitions')
+
+    sub = parser.add_subparsers(dest='action')
+
+    dump = sub.add_parser('dump', help='Dump repo labels to local file')
+    dump.add_argument('-f', '--file', type=pathlib.Path, help='File path to dump labels to')
+
+    update = sub.add_parser('update', help='Update repo labels using the specified label definition file')
+    update.add_argument('-f', '--file', type=pathlib.Path, help='Path to file containing label definitions')
+
+    # # optional arguments
+    # optional.add_argument('-t', '--token', type=str, help='GitHub Token (required if not specified as GITHUB_ACCESS_TOKEN environment variable or as a file')
+    # optional.add_argument('-T', '--token-file', type=str, help='Path to file containing GitHub Token')
+    #
+    # #optional.add_argument('-f', '--file', type=pathlib.Path, help='Path to file required for action (required when using "dump" or "update" action commands)')
+    #
+    # # required arguments
+    # required.add_argument('-o', '--owner', type=str, help='Specify user / organisation who owns the repository')
+    # required.add_argument('-r', '--repo', type=str, help='Specify name of the repository')
+    #
+    # parser.add_argument('action', type=str, choices=['clear', 'config', 'dump', 'get', 'update'], help='Action to perform')
 
     return parser
 
@@ -27,7 +53,7 @@ def _process_args() -> None:
     parser: argparse.ArgumentParser = _parser()
     args: argparse.Namespace = parser.parse_args()
 
-def run():
+def run() -> None:
     _process_args()
 
 if __name__ == "__main__":

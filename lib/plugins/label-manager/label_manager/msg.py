e6a30d071a771a2185762621a7e8be1aade4bcb3
Ragdata <ragdata@users.noreply.github.com>
2024-10-20 01:40:23 +1000

    fix: Remove @staticmethod declaration from class methods

--- lib/plugins/label-manager/label_manager/msg.py
+++ lib/plugins/label-manager/label_manager/msg.py
@@ -1,3 +1,18 @@
+####################################################################
+# label_manager.msg.py
+####################################################################
+# GitHub Repository Label Manager
+#
+# File:         msg.py
+# Package:      label_manager
+# Author:       Ragdata
+# Date:         14/10/2024
+# License:      MIT License
+# Repository:	https://github.com/Ragdata/.dotfiles
+# Copyright:    Copyright © 2024 Redeyed Technologies
+####################################################################
+# DEPENDENCIES
+####################################################################
 import colorama
 
 class msg:
@@ -9,18 +24,14 @@ class msg:
     BOLD: str = colorama.Style.BRIGHT
     RESET: str = colorama.Style.RESET_ALL
 
-    @staticmethod
     def success(self, message: str) -> None:
         print(f'{self.BOLD}{self.SUCCESS}{message}{self.RESET}')
 
-    @staticmethod
     def warn(self, message: str) -> None:
         print(f'{self.BOLD}{self.WARN}{message}{self.RESET}')
 
-    @staticmethod
     def error(self, message: str) -> None:
         print(f'{self.BOLD}{self.ERROR}{message}{self.RESET}')
 
-    @staticmethod
     def info(self, message: str) -> None:
         print(f'{self.BOLD}{self.INFO}{message}{self.RESET}')

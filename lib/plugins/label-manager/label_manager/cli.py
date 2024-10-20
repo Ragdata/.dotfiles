387eba6eb10ab7324be70daa55848717f86debde
Ragdata <ragdata@users.noreply.github.com>
2024-10-20 10:31:24 +1000

    feat: Update label_manager plugin

--- lib/plugins/label-manager/label_manager/cli.py
+++ lib/plugins/label-manager/label_manager/cli.py
@@ -1,51 +1,68 @@
+####################################################################
+# label_manager.cli.py
+####################################################################
+# GitHub Repository Label Manager
+#
+# File:         cli.py
+# Package:      label_manager
+# Author:       Ragdata
+# Date:         14/10/2024
+# License:      MIT License
+# Repository:	https://github.com/Ragdata/.dotfiles
+# Copyright:    Copyright © 2024 Redeyed Technologies
+####################################################################
+# DEPENDENCIES
+####################################################################
 import argparse
-import pathlib
-import logging
-import random
-import json
-import sys
-import os
-import re
 
-from label_manager import manager, msg
+from pathlib import Path
 
 def _parser() -> argparse.ArgumentParser:
+
     parser = argparse.ArgumentParser(
         prog="label_manager",
-        description="Manage labels for a GitHub Repository",
+        description="Manage GitHub Repository Labels",
         add_help=False,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
 
     flags = parser.add_argument_group('flags')
     flags_mex = flags.add_mutually_exclusive_group()
-    optional = parser.add_argument_group('optional')
-    required = parser.add_argument_group('required')
+    options = parser.add_argument_group('options')
 
-    # cli flags
+    parser.add_argument('action', type=str, nargs='?', help='Action to perform')
+
     flags_mex.add_argument('-h', '--help', action='help', help='Show this help message and exit')
-    flags_mex.add_argument('-V', '--version', action='version', version="%(prog)s v0.1.0", help='Display module version and exit')
-    flags_mex.add_argument('-v', '--validate', action='store_true', help='Validate local label definitions')
+    flags_mex.add_argument('-V', '--version', action='version', version='%(prog)s ver 0.1.0', help='Display module version and exit')
 
-    # optional arguments
-    optional.add_argument('-t', '--token', type=str, help='GitHub Token (required if not specified as GITHUB_ACCESS_TOKEN environment variable or as a file')
-    optional.add_argument('-T', '--token-file', type=str, help='Path to file containing GitHub Token')
-    optional.add_argument('-f', '--file', type=pathlib.Path, help='Path to file required for action (required when using "dump" or "update" action commands)')
-
-    # required arguments
-    required.add_argument('-o', '--owner', type=str, help='Specify user / organisation who owns the repository')
-    required.add_argument('-r', '--repo', type=str, help='Specify name of the repository')
-
-    parser.add_argument('action', type=str, choices=['clear', 'config', 'dump', 'get', 'update'], help='Action to perform')
+    options.add_argument('-t', '--token', help='GitHub Token or path to file containing token (required if no GITHUB_ACCESS_TOKEN environment variable)')
+    options.add_argument('-f', '--file', type=Path, help='Path to file required by action (required for "dump", "update", and "validate" actions)')
+    options.add_argument('-o', '--owner', type=str, help='Specify user / organisation')
+    options.add_argument('-r', '--repo', type=str, help='Specify repository name')
 
     return parser
 
 def _process_args() -> None:
+
     parser: argparse.ArgumentParser = _parser()
     args: argparse.Namespace = parser.parse_args()
+
+    match args.action:
+        case "clear":
+            print('clear')
+        case "config":
+            print('config')
+        case "dump":
+            print('dump')
+        case "get":
+            print('get')
+        case "update":
+            print('update')
+        case "validate":
+            print('validate')
 
 def run() -> None:
     _process_args()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     run()
---
+++ lib/plugins/label-manager/label_manager/manager.py
@@ -0,0 +1,15 @@
+####################################################################
+# label_manager.manager.py
+####################################################################
+# GitHub Repository Label Manager
+#
+# File:         manager.py
+# Package:      label_manager
+# Author:       Ragdata
+# Date:         14/10/2024
+# License:      MIT License
+# Repository:	https://github.com/Ragdata/.dotfiles
+# Copyright:    Copyright © 2024 Redeyed Technologies
+####################################################################
+# DEPENDENCIES
+####################################################################
